474f80153d4727303db3017aad9ade907da434cb
nicoviarnes <nlviarnes@gmail.com>
2018-01-08 15:36:40 -0800

    add oauth2 logic

---
+++ config/prod.js
@@ -0,0 +1,6 @@
+module.exports = {
+	googleClientID: process.env.GOOGLE_CLIENT_ID,
+	googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+	cookieKey: process.env.MONGO_URI,
+	mongoURI: process.env.COOKIE_KEY
+};
