bda789c61244a1b2fbf7b7c5c418d9ccc9a1ef33
nicoviarnes <nlviarnes@gmail.com>
2018-01-10 13:03:28 -0800

    add heroku build config

--- index.js
+++ index.js
@@ -25,5 +25,14 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
+if (proces.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
+
+	const path = require('path');
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build'));
+	});
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
