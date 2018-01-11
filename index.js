1d42b2ffcddfa8ee142f9267449ca8835e8eaf62
nicoviarnes <nlviarnes@gmail.com>
2018-01-10 18:28:55 -0800

    tweak routes

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ require('./routes/billingRoutes')(app);
 
 if (process.env.NODE_ENV === 'production') {
 	app.use(express.static('client/build'));
-	app.get('*', (req, res) => {
+	app.get('/**', (req, res) => {
 		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 	});
 }
