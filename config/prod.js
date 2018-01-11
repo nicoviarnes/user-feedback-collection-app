20406a689782be332f54ca025e1925409b058362
nicoviarnes <nlviarnes@gmail.com>
2018-01-10 17:32:58 -0800

    fix typo in prod.js that was causing issue with writing to mongodb

--- config/prod.js
+++ config/prod.js
@@ -1,8 +1,8 @@
 module.exports = {
 	googleClientID: process.env.GOOGLE_CLIENT_ID,
 	googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-	cookieKey: process.env.MONGO_URI,
-	mongoURI: process.env.COOKIE_KEY,
+	cookieKey: process.env.COOKIE_KEY,
+	mongoURI: process.env.MONGO_URI,
 	stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
 	stripeSecretKey: process.env.STRIPE_SECRET_KEY
 };
